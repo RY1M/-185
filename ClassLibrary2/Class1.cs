5a71f81bd587e1e64603bcc259c34f415ac51798
RY1M <90375719+RY1M@users.noreply.github.com>
2022-03-10 16:00:59 +0300

    Add files via upload

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassLibrary2
+{
+    public class vscode
+    {
+        public static string GetStundNumber(string student, DateTime date, int group)
+        {
+            var FullName = student.Split(' ');
+            return $"{date.ToString("yyyy")}.{group}.{FullName[0]} {FullName[1].Substring(0, 1)}.{FullName[2].Substring(0, 1)}";
+        }
+        public void add(int a, int b)
+        {
+            IEnumerable<int> nums = Enumerable.Range(a, b);
+            Console.WriteLine("следовательность чисел: \n");
+
+            foreach (int i in nums)
+                Console.Write(i);
+            foreach (int k in nums)
+                Console.Write(k);
+
+            double average = nums.Average();
+            double min = nums.Min();
+            Console.WriteLine("\n\nСреднее арифмитическое: " + average);
+            Console.WriteLine("\n\nСреднее арифмитическое: " + min);
+
+        }
+    }
+}
